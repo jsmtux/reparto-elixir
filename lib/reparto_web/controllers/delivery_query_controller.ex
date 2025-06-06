8a4f1a781a5f3cfedea9a734f1af076bccd44cde
jsmtux <jsmtux@gmail.com>
2025-06-06 09:45:30 +0200

    Query results now show routes and companies that can be visited

--- lib/reparto_web/controllers/delivery_query_controller.ex
+++ lib/reparto_web/controllers/delivery_query_controller.ex
@@ -2,11 +2,12 @@ defmodule RepartoWeb.DeliveryQueryController do
   use RepartoWeb, :controller
 
   alias Reparto.Coverage
+  alias Reparto.Repo
 
   @spec create(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def create(conn, %{"lat" => lat, "lon" => lon}) do
     point = %Geo.Point{coordinates: {lat, lon}, srid: 4326}
-    routes = Coverage.list_routes_close_to(point)
+    routes = Coverage.list_routes_close_to(point) |> Repo.preload(:company)
     render(conn, :index, lat: lat, lon: lon, routes: routes)
   end
 end
